01a8ec0305c9b21b2e70996e4d08dc9c923880fa
euglazer <eugenestapler@gmail.com>
2015-04-22 01:37:43 +1200

    lol yep this one

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 $(document).ready(function () {
 
-  for (var i = 1; i < 30; i++) {
+  for (var i = 1; i < 100; i++) {
     var color = ( i % 2 === 0 ? 'red' : 'black');
     $circle = $("<div class='centered circle'></div>").css({
       'background' : color,
-      'height': 24 * i,
-      'width': 24 * i,
+      'height': 5* i,
+      'width': 5* i,
       'z-index' : -1 * i
     });
     $('body').append($circle);
   }
 
-  for (var i = 0; i < 30; i++) {
-    var $dot = $("<div class='centered dot'></div>")
-    $('body').append($dot);
-  }
+  // for (var i = 0; i < 100; i++) {
+  //   var $dot = $("<div class='centered dot'></div>")
+  //   $('body').append($dot);
+  // }
 
-  $.each($('.dot'), function (index, value) {
+  // $.each($('.dot'), function (index, value) {
 
-    var degrees = 0;
+  //   var degrees = 0;
 
-    setInterval(function () {
-      var radius = 24 * index + 12;
-      var x = Math.round(radius * Math.cos(degrees * Math.PI / 180));
-      var y = Math.round(radius * Math.sin(degrees * Math.PI / 180));
-      $(value).css({
-        'top' : y,
-        'left' : x
-      });
-      degrees++;
-    }, index + 15);
+  //   setInterval(function () {
+  //     var radius = 5* index + 12;
+  //     var x = Math.round(radius * Math.cos(degrees * Math.PI / 180));
+  //     var y = Math.round(radius * Math.sin(degrees * Math.PI / 180));
+  //     $(value).css({
+  //       'top' : y,
+  //       'left' : x
+  //     });
+  //     degrees++;
+  //   }, index + 30);
 
-  });
+  // });
 
   $.each($('.circle'), function (index, value) {
     var degrees = 0;
@@ -40,13 +40,13 @@ $(document).ready(function () {
         'webkit-transform' : 'rotate(-' + degrees + 'deg)'
       })
       degrees++;
-    }, index + 15)
+    }, index + 30)
   });
 
   var degrees = 0;
   setInterval(function () {
-    $('body').css({'webkit-transform' : 'rotate(-' + 100 * degrees + 'deg)'});
+    $('body').css({'webkit-transform' : 'rotate(-' + 5 * degrees + 'deg)'});
     degrees++;
-  }, 15)
+  }, 30)
 
 });
