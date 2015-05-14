6e45d2ed75fbd798bbc5d9575bab65eada9c2972
euglazer <eugenestapler@gmail.com>
2015-05-14 12:04:37 +1200

    reorganize file structure

--- javascripts/index.js
+++ javascripts/index.js
@@ -11,28 +11,6 @@ $(document).ready(function () {
     $('body').append($circle);
   }
 
-  // for (var i = 0; i < 100; i++) {
-  //   var $dot = $("<div class='centered dot'></div>")
-  //   $('body').append($dot);
-  // }
-
-  // $.each($('.dot'), function (index, value) {
-
-  //   var degrees = 0;
-
-  //   setInterval(function () {
-  //     var radius = 5* index + 12;
-  //     var x = Math.round(radius * Math.cos(degrees * Math.PI / 180));
-  //     var y = Math.round(radius * Math.sin(degrees * Math.PI / 180));
-  //     $(value).css({
-  //       'top' : y,
-  //       'left' : x
-  //     });
-  //     degrees++;
-  //   }, index + 30);
-
-  // });
-
   $.each($('.circle'), function (index, value) {
     var degrees = 0;
     setInterval(function () {
